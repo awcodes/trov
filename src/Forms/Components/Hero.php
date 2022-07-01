82be7ef666049e1bfe106a2b161a1954c64db295
awcodes <adam@adamwgd.com>
2022-07-01 16:02:47 -0400

    wip

--- src/Forms/Components/Hero.php
+++ src/Forms/Components/Hero.php
@@ -3,6 +3,7 @@
 namespace Trov\Forms\Components;
 
 use Closure;
+use Filament\Forms\Components\Radio;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Textarea;
@@ -10,6 +11,8 @@ use Illuminate\Database\Eloquent\Model;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
 use FilamentAddons\Forms\Fields\VideoEmbed;
+use FilamentAddons\Forms\Components\VimeoEmbed;
+use FilamentAddons\Forms\Components\YouTubeEmbed;
 use FilamentCurator\Forms\Components\MediaPicker;
 
 class Hero
@@ -18,17 +21,24 @@ class Hero
     {
         return Section::make('Hero')
             ->schema([
-                Toggle::make('hero.is_video')
-                    ->label('Is Video')
+                Radio::make('hero.type')
+                    ->inline()
+                    ->default('image')
+                    ->options([
+                        'image' => 'Image',
+                        'youtube' => 'YouTube',
+                        'vimeo' => 'Vimeo'
+                    ])
                     ->reactive(),
                 MediaPicker::make('hero.image')
                     ->label('Image')
-                    ->hidden(fn (Closure $get): bool => $get('hero.is_video') ?: false),
-                VideoEmbed::make('hero.video')
-                    ->label('Embed Code')
-                    ->visible(fn (Closure $get): bool => $get('hero.is_video') ?: false)
-                    ->rows(3)
-                    ->reactive(),
+                    ->visible(fn (Closure $get): bool => $get('hero.type') == 'image' ?: false),
+                YouTubeEmbed::make('hero.youtube')
+                    ->label('YouTube Embed')
+                    ->visible(fn (Closure $get): bool => $get('hero.type') == 'youtube' ?: false),
+                VimeoEmbed::make('hero.vimeo')
+                    ->label('YouTube Embed')
+                    ->visible(fn (Closure $get): bool => $get('hero.type') == 'vimeo' ?: false),
                 Textarea::make('hero.cta')
                     ->label('Call to Action')
                     ->rows(3),
